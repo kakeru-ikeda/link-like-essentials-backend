2d27a547015cf0827125441d0c0a9ca16931bed5
kakeru-ikeda <kakerumail1451@gmail.com>
2025-12-05 00:26:21 +0900

    [LLES-51] CardモデルにreleaseDateフィールドを追加し、CardRepositoryのソートロジックを更新

--- src/domain/entities/Card.ts
+++ src/domain/entities/Card.ts
@@ -9,6 +9,7 @@ export interface Card {
   cardUrl: string | null;
   characterName: string;
   styleType: string | null;
+  releaseDate: Date | null;
   isLocked: boolean | null;
   createdAt: Date | null;
   updatedAt: Date | null;
